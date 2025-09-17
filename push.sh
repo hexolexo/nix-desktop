2d0317ee6867627104a860d66a928206eb3a893e
hexolexo <hexolexo132@proton.me>
2025-09-17 21:13:08 +1000

    Updated push.sh

--- push.sh
+++ push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Pushing changes..."
 git push "$@"
 if [ $? -eq 0 ]; then
-    echo "=== Push successful, installing desktop ==="
     sudo sh -c 'cd /etc/nixos && git pull && nix-channel --update && nixos-rebuild switch' && timeout 5 git push github master
 else
     echo "Push failed, deployment aborted"
